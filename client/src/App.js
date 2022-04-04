b9bbe2bab2bd16ad23a77c587b1478f71806ccf0
cho younghyun <5869jyh@hanmail.net>
2022-04-04 18:07:06 +0900

    리스트 뿌릭 커밋

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,39 @@
 
 
 import axios from 'axios';
-import {useEffect} from "react";
+import React, { useEffect, useState } from "react";
 import './App.css';
+import './assets/css/market.css';
+
 
 function App() {
- 
-    const callApi = async () => {
-        axios.get("/api").then((res) => 
-            console.log(res.data.rows)
-        );
-    };
+
+    const [list, setList] = useState([]);
+
+    const getHeroes = async () => {
+        let response = await axios.get('/api');
+
+        setList(response.data.rows);
+    }
+
 
     useEffect(() => {
-      callApi();
+        getHeroes();
     }, []);
 
-    return <div>테스트 입니다. </div>;
+    return (
+        <div>
+            <h1>Our new Products</h1>
+                <div class="products">
+                {list.map(list => (
+                <a href="#">
+                    <img src={list.photo} alt={list.name}/>
+                    <p>{list.brdtitle}</p>
+                    <p class="price">{list.brdno}</p>
+                </a>
+                ))}
+            </div>
+        </div>
+    )
 }
-
 export default App;
