de773c97511e5c54f9f20d6b5b6cda96db4ea123
cho younghyun <5869jyh@hanmail.net>
2022-04-22 17:57:07 +0900

    머신러닝 학습 환경 세팅(node + python)

--- client/src/view/prodList.js
+++ client/src/view/prodList.js
@@ -19,7 +19,7 @@ function ProdList() {
     //추천 상품 리스트 호출 머신러닝
     const getNdcg = async () => {
 
-        let response = await axios.get('/api/nodemlMovie2');
+        //let response = await axios.get('/api/moviesDataInit');
     }
 
     useEffect(() => {
@@ -37,15 +37,15 @@ function ProdList() {
 
                 <a href="#" key={list.brdno} className="text-center" >    
                 <div className={"card border-"+ color[Number(index%6)] +" col-md-5 m-4"} >
-                    <div className={"card-header bg-"+ color[Number(index%6)]}>{list.brdtitle}</div>
+                    <div className={"card-header bg-"+ color[Number(index%6)]}>{list.original_title}</div>
                     <div className={"card-body text-" + color[Number(index%6)]} >
                         <img src={"https://source.unsplash.com/random/200x200?sig="+index} alt ="not exist" />
                         {/* 
                             랜덤 이미지 링크 2
                             <img src={"https://picsum.photos/200/300?random="+index} alt ="not exist" /> 
                         */}
-                        <h5 className="card-title">{list.brddate}</h5>
-                        <p className="card-text">{list.brdwriter}</p>
+                        <h5 className="card-title">{list.release_date}</h5>
+                        <p className="card-text">{list.overview}</p>
                     </div>
                 </div>
                 </a>    
--- server/Router/test.js
+++ server/Router/test.js
@@ -2,20 +2,40 @@ const express = require("express");
 const router = express.Router();
 
 var firebase = require("firebase");
-var dateFormat = require('dateformat');
 
-/*  다이어리
-var config = {
-    apiKey: "AIzaSyD_tg-m8Y77Q7K_w2lyY68igE4RWy-63DY",
-    authDomain: "nodejs-54f7b.firebaseapp.com",
-    databaseURL: "https://nodejs-54f7b-default-rtdb.firebaseio.com",
-    projectId: "nodejs-54f7b",
-    storageBucket: "nodejs-54f7b.appspot.com",
-    messagingSenderId: "647841088954",
-    appId: "1:647841088954:web:03a1abd6bf64a363d7c983",
-    measurementId: "G-4LBL09LTYG"
-};
-*/
+var dateFormat = require('dateformat');
+const csv = require('csv-parser');
+const fs = require('fs');
+
+var evaluate = require('eval-literals');//문자열을 리스트 객체로 변환
+
+
+
+
+const dfd = require("danfojs");
+const tf = require("@tensorflow/tfjs");//느리다... @tensorflow/tfjs_node로 업데이트 필요
+//const tf = dfd.tensorflow;
+
+
+
+const PythonShell = require("python-shell");
+var options = {
+
+    mode: 'text',
+  
+    pythonPath: '',
+  
+    pythonOptions: ['-u'],
+  
+    scriptPath: '',
+  
+    args: ['value1', 'value2', 'value3']
+  
+  };
+
+
+
+
 /* 영화 */
 const config = {
     apiKey: "AIzaSyCzWDbX5Qad0iyaxAeam45rUJxkV41yFPs",
@@ -32,16 +52,111 @@ var db = firebase.firestore();
 
 
 router.get("/", (req, res) => {
+    
     /*
-    db.collection('board').orderBy("brddate","desc").get()
+    const {ig} = require('ignore');
+    const pd = require("node-pandas-js")
+
+    var moviesResult = [];
+    
+    fs.createReadStream('C:/Users/all4land/Desktop/NodeJS-FireBase-React/client/src/data/movie/tmdb_5000_movies copy.csv')
+    .pipe(csv())
+    .on('data', (data) =>{ moviesResult.push(data) })
+    .on('end', () => {
+        var df = pd.DataFrame(moviesResult);
+        console.log(df);
+    });
+    */
+
+
+    var moviesResult = [];
+    fs.createReadStream('C:/Users/all4land/Desktop/NodeJS-FireBase-React/client/src/data/movie/tmdb_5000_movies copy.csv')
+    .pipe(csv())
+    .on('data', (data) =>{ moviesResult.push(data) })
+    .on('end', () => {
+        
+        //let tensor_arr = tf.tensor(moviesResult);
+        var df = new  dfd.DataFrame(moviesResult);
+        
+
+
+        /*
+            ※ 참조 링크 :  https://danfo.jsdata.org/getting-started
+            concat : 데이터 프레임들을 연결
+            mean   : 숫자를 소수점 까지 표현
+            merge  : 데이터 프레임 합치기
+            monthName : 날자 폼에서 월 명을 반환 1 -> January
+
+            ★★ 그외 스크립트에서 cvs를 실행하여 통계, 차트 등의 관측이 가능 하다. ★★
+        */
+        df.print();//테이블 형식으로 출력
+        df.iloc({ rows: [":"], columns: ["1:2"]}).print();//모든열 두번쩨 컬럼 반환
+        df.iloc({ rows: ["0:2"], columns: [":"]}).print();//2열까지 모든 컬럼 반환
+        df.iloc({ rows: df["vote_average"].gt(7) }).print();//해당 컬럼이 7보다 큰 열 반환
+        df.iloc({ rows: df["vote_average"].gt(7).and(df["title"].eq("Avatar")), columns: [0]}).print();//해당컬럼이 10보다 크고 이름이 사과인것 1열 반환
+        df.isNa().print();//열에서 NaN 는 true 나머지 false 반환
+        console.log(df.tensor);//전체 데이터 프레임 출력
+
+
+        
+          // 1. 데이터를 준비
+          var movieDf = df.loc({ columns: ['id','title','genres','vote_average', 'vote_count', 'popularity', 'keywords', 'overview']} )
+        
+          PythonShell.PythonShell.run ('C:/Users/all4land/Desktop/NodeJS-FireBase-React/server/Router/test1.py', options, function (err, results) {
+            
+            if (err) {
+                console.log("통신실패");
+                console.log(err);
+            
+            }   
+            else{
+                console.log("통신성공");
+                console.log(results);
+            } 
+
+            //console.log('results: %j', results);
+            console.log("★★★★★★★");
+        });
+    });
+
+
+
+
+    
+    /*
+    var moviesResult2 = [];
+    db.collection('movies').orderBy("release_date","desc").get()
     .then((snapshot) => {
         var rows = [];
+
         snapshot.forEach((doc) => {
             var childData = doc.data();
-            childData.brddate = dateFormat(childData.brddate,"yyyy-mm-dd");
+            childData.release_date = dateFormat(childData.release_date,"yyyy-mm-dd");
+
             rows.push(childData);
+            moviesResult2.push(childData);
+             
         });
+
+        const pd2 = require("node-pandas-js")
+        var df = pd2.DataFrame(moviesResult2);
+        var movies_df = df['id','title','genres','vote_average', 'vote_count', 'popularity', 'keywords', 'overview']
+        console.log(movies_df);
+
+        //evaluate
+        
+        //movies_df['genres'] = eval(movies_df['genres'])
+        //movies_df['keywords'] = movies_df['keywords'].apply(evaluate)
+        console.log(movies_df['genres']);
+        
+        movies_df['genres'] = movies_df['genres'].apply(lambda x : [y['name'] for y in x])
+        movies_df['keywords'] = movies_df['keywords'].apply(lambda x : [y['name'] for y in x])
+        print(movies_df['genres'].head(1))
+        print(movies_df['keywords'].head(1))
+        
+       
         res.send( {rows: rows});
+        
     })
     .catch((err) => {
         console.log('Error getting documents', err);
@@ -55,21 +170,6 @@ router.get("/", (req, res) => {
     1. User-based CF (ex > 넷플릭스 : 사용자의 시청 영화에 따라 영화 추천 )
 */
 router.get("/nodemlMovie", (req, res) => {
-
-    const csv = require('csv-parser')
-    const fs = require('fs')
-    const resultsqw = [];
-
-    fs.createReadStream('C:/Users/all4land/Desktop/NodeJS-FireBase-React/client/src/data/movie/tmdb_5000_movies.csv')
-    .pipe(csv())
-    .on('data', (data) => resultsqw.push(data))
-    .on('end', () => {
-        console.log(resultsqw);
-        // [
-        //   { NAME: 'Daffy Duck', AGE: '24' },
-        //   { NAME: 'Bugs Bunny', AGE: '22' }
-        // ]
-    });
 
     const {Bayes} = require('nodeml');
     let bayes = new Bayes();
@@ -112,7 +212,7 @@ router.get("/nodemlMovie", (req, res) => {
 
     let ndcg = evaluation.ndcg(gt, result);
     console.log(gt);
-    console.log(result);-
+    console.log(result);
     console.log(ndcg);
 
     res.send({test : "hi"});
@@ -120,23 +220,19 @@ router.get("/nodemlMovie", (req, res) => {
 
 
 /*
-    추천 상품 머신러닝 (참조링크  : https://proinlab.com/archives/2103 , https://www.npmjs.com/package/nodeml)
-    1. User-based CF (ex > 넷플릭스 : 사용자의 시청 영화에 따라 영화 추천 )
+    영화 추천 시스템 구축을 위한 데이터 초기화 컨트롤러 ( 원천 데이터 : https://www.kaggle.com/datasets/tmdb/tmdb-movie-metadata?resource=download )
 */
-router.get("/nodemlMovie2", (req, res) => {
-
-    const csv = require('csv-parser');
-    const fs = require('fs');
+router.get("/moviesDataInit", (req, res) => {
     
-    const movieSResult = [];
+    const moviesResult = [];
     const creditsRsult = [];
     /* 영화 정보 */
     fs.createReadStream('C:/Users/all4land/Desktop/NodeJS-FireBase-React/client/src/data/movie/tmdb_5000_movies.csv')
     .pipe(csv())
-    .on('data', (data) => movieSResult.push(data))
+    .on('data', (data) => moviesResult.push(data))
     .on('end', () => {
         count = 0 
-        movieSResult.forEach((doc) => {
+        moviesResult.forEach((doc) => {
 
             if(count > 20) return false
             count++
@@ -144,31 +240,30 @@ router.get("/nodemlMovie2", (req, res) => {
             var movieDoc = db.collection("movies").doc();
 
             var postData = {
-                id: movieDoc.id,
-                movieId : doc.id,
-                title   : doc.title,
-                budget     : doc.budget,
-                genres  : doc.genres,
-                homepage   : doc.homepage,
-                original_language   : doc.original_language,
-                original_title : doc.original_title,
-                overview     : doc.overview,
-                popularity  : doc.popularity,
-                production_companies   : doc.production_companies,
-                production_countries   : doc.production_countries,
-                release_date : doc.release_date,
-                revenue     : doc.revenue,
-                runtime  : doc.runtime,
-                spoken_languages   : doc.spoken_languages,
-                status   : doc.status,
-                tagline   : doc.tagline,
-                vote_average   : doc.vote_average,
-                vote_count   : doc.vote_count
+                id                   : movieDoc.id,
+                movie_id             : doc.id,
+                title                : doc.title,
+                budget               : doc.budget,
+                genres               : doc.genres,
+                homepage             : doc.homepage,
+                original_language    : doc.original_language,
+                original_title       : doc.original_title,
+                overview             : doc.overview,
+                popularity           : doc.popularity,
+                production_companies : doc.production_companies,
+                production_countries : doc.production_countries,
+                release_date         : doc.release_date,
+                revenue              : doc.revenue,
+                runtime              : doc.runtime,
+                spoken_languages     : doc.spoken_languages,
+                status               : doc.status,
+                tagline              : doc.tagline,
+                vote_average         : doc.vote_average,
+                vote_count           : doc.vote_count
             };
             movieDoc.set(postData);
-            
         });
-        
+        res.json({data : moviesResult});
     });
     
     /*  출연진 정보 */
@@ -177,7 +272,6 @@ router.get("/nodemlMovie2", (req, res) => {
     .on('data', (data) => creditsRsult.push(data))
     .on('end', () => {
         count = 0 
-        
 
         creditsRsult.forEach((doc) => {
 
@@ -187,19 +281,16 @@ router.get("/nodemlMovie2", (req, res) => {
             var creditsDoc = db.collection("credits").doc();
 
                 var postData = {
-                    id: creditsDoc.id,
-                    movieId : doc.movie_id,
-                    title   : doc.title,
-                    budget     : doc.cast,
-                    genres  : doc.crew,
-
+                    id       : creditsDoc.id,
+                    movie_id : doc.movie_id,
+                    title    : doc.title,
+                    cast     : doc.cast,
+                    crew     : doc.crew,
                 };
                 creditsDoc.set(postData);
-        
         });
     });
-
-    res.json({data : creditsRsult});
+    
 });
 
 
