2eca0b9459282f79fb165c4944fbf221ce7d4f64
jyh5869 <5869jyh@hanmail.net>
2022-10-27 01:05:35 +0900

    home_페이징 공통처리 마무리

--- client/src/view/common/pagination.js
+++ client/src/view/common/pagination.js
@@ -1,96 +1,50 @@
 
-import React, { useEffect, useState, Component} from "react";
-
-import ToggleButton from 'react-bootstrap/ToggleButton';
-import ButtonGroup from 'react-bootstrap/ButtonGroup';
-
-import axios from 'axios'
-
 /**
- * @returns prevDoc, nextDoc
+ * @author 공통 페이징 처리 함수
+ * @returns 받은 파라메터를 그대로 [prevDoc, nextDoc, docList, prevTarget]
+ * @param data
  * @param type
+ * @param docList
+ * @param prevTarget 
 */
-export default function Pagination (props) {
-    console.log(props);
-    let   [next     , setNext]     = useState("");
-    let   [prev     , setPrev]     = useState("");
+export default function Pagination (data, type, docList, prevTarget ) {
 
-    let type       = props.useParams.type == undefined ? '' : props.useParams.type 
-    let docList    = props.useParams.docList == undefined ? [] : props.useParams.docList
-    let data       = props.useParams.data == undefined ? [] : props.useParams.data
-    let prevTarget = props.useParams.prevTarget == NaN ? 0 : props.useParams.prevTarget;
-    let url        = props.useParams.url;
-    let docId      = type == "next" ? next : prev
+    let prevDoc;
+    let nextDoc;
+    
+    //이전 페이지를 호출 할 경우
+    if(type == "prev"){
+        prevTarget++//이전페이지 카운트 증가
 
-
-    //리스트 가져오기
-    const getMovieList = async (useParams, e) => {
-        alert("함수호출");
-        let type       = useParams.type == undefined ? '' : useParams.type 
-        let docList    = useParams.docList == undefined ? [] : useParams.docList
-        let docId      = useParams.docId;
-        let prevTarget = useParams.prevTarget == NaN ? 0 : useParams.prevTarget;
-        
-        let response = await axios({
-            method  : 'get',
-            url     : '/api',
-            params  : {
-                'doc_id' : docId ,
-                'type'   : type
-            },
-            headers : {
-                'Content-Type' : 'multipart/form-data'
-            },
-        })
-
-        var data = response.data.rows
-        
-        let prevDoc;
-        let nextDoc;
-        
-        //이전 페이지를 호출 할 경우
-        if(type == "prev"){
-            prevTarget++//이전페이지 카운트 증가
-
-            //첫페이지에서 이전 페이지 호출(이전페이지 카운트가 저장 리스트의 길이와 같을 경우)
-            if(prevTarget == docList.length - 1 ){
-                prevTarget--//이전페이지 카운트 감소(이후 이전 페이지 호출시에도 첫페이지를 호출 하기 위함)
-            }
-
-        }//다음 페이지를 호출 할 경우
-        else if(type == "next" || type == ""){
-            docList.push(data[0].doc_id)//호출한 페이지의 문서 아이디값 추가
-
-            //배열 중복 제거(만약 다음 페이지가 이전페이지 호출등의 이유로 이미 호출 했을 경우 배열에 쌓지 않기 위함)
-            const set1 = new Set(docList);
-            
-            if(prevTarget != 0 ) {//첫페이지가 아닐 경우
-                prevTarget-- //이전페이지 카운트 감소
-            }
+        //첫페이지에서 이전 페이지 호출(이전페이지 카운트가 저장 리스트의 길이와 같을 경우)
+        if(prevTarget == docList.length - 1 ){
+            prevTarget--//이전페이지 카운트 감소(이후 이전 페이지 호출시에도 첫페이지를 호출 하기 위함)
         }
 
-        //다음페이지 눌렸을때 게시물 수가 countPerPage 이하힐때 마지막페이지로 간주
-        if(type == "next" && data.length < 10){  
-            nextDoc = data[0].doc_id
-            prevDoc = type == "" ? docList[0] : docList[docList.length - (prevTarget + 2)]
-        }
-        else{//마지막 페이지가 아닐때
-            nextDoc = data[Number(data.length-1)].doc_id
-            prevDoc = type == "" ? docList[0] : docList[docList.length - (prevTarget + 2)]
-        }
-        
-        console.log("prevTarget = " + prevTarget)
+    }//다음 페이지를 호출 할 경우
+    else if(type == "next" || type == ""){
+        docList.push(data[0].doc_id)//호출한 페이지의 문서 아이디값 추가
 
+        //배열 중복 제거(만약 다음 페이지가 이전페이지 호출등의 이유로 이미 호출 했을 경우 배열에 쌓지 않기 위함)
+        const set1 = new Set(docList);
+        docList = [...set1];
+
+        if(prevTarget != 0 ) {//첫페이지가 아닐 경우
+            prevTarget-- //이전페이지 카운트 감소
+        }
     }
+    //다음페이지 눌렸을때 게시물 수가 countPerPage 이하힐때 마지막페이지로 간주
+    if(type == "next" && data.length < 10){  
+        nextDoc = data[0].doc_id
+        prevDoc = type == "" ? docList[0] : docList[docList.length - (prevTarget + 2)]
+    }
+    else{//마지막 페이지가 아닐때
+        nextDoc = data[Number(data.length-1)].doc_id
+        prevDoc = type == "" ? docList[0] : docList[docList.length - (prevTarget + 2)]
+    }
+    
+    let pagingArr = [prevDoc, nextDoc, docList, prevTarget]
 
-    
-    
-    return (
-        <div className="pagination_wrap">
-            <ButtonGroup className="pagination">
-                <ToggleButton onClick={(e)=>{getMovieList({ type : 'prev', docId : prevDoc, docList : docList, prevTarget: prevTarget}, e)}} type="radio"variant={'outline-success'} name="radio"> &larr; 이전 </ToggleButton>
-                <ToggleButton onClick={(e)=>{getMovieList({ type : 'next', docId : nextDoc, docList : docList, prevTarget: prevTarget}, e)}} type="radio"variant={'outline-primary'} name="radio"> 다음 &rarr; </ToggleButton>
-            </ButtonGroup>
-        </div>
-    )
+    return pagingArr;
+
 }
--- client/src/view/movie/movieList.js
+++ client/src/view/movie/movieList.js
@@ -5,23 +5,19 @@ import axios from 'axios';
 import ToggleButton from 'react-bootstrap/ToggleButton';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 
-// import Pagination from '../common/pagination';
+import Pagination from '../common/pagination';
 
 /**
- * 영화 리스트 페이지
+ * @author 영화 리스트 페이지
  * @returns
 */
-let docList = [];
-let docListId = [];
-// let prevTarget = 0;
-
 function MovieList() {
 
-    const [list     , setList]     = useState([]);
-    const [docList      , setDocList]  = useState();
-    let   [prevTarget     , setPrevTarget]     = useState("");
-    let   [next     , setNext]     = useState("");
-    let   [prev     , setPrev]     = useState("");
+    const [list      , setList      ] = useState([]);
+    const [docList   , setDocList   ] = useState();
+    let   [prevTarget, setPrevTarget] = useState("");
+    let   [next      , setNext      ] = useState("");
+    let   [prev      , setPrev      ] = useState("");
     
     const radios = [
         { name: 'Active', value: '1' },
@@ -29,87 +25,15 @@ function MovieList() {
       ];
 
     var color = [ "danger", "warning", "info", "primary", "secondary", "success" ];
-    
+
     //리스트 가져오기
     const getMovieList = async (useParams, e) => {
         
-        let type  = useParams.type == undefined ? '' : useParams.type 
-        let docId = type == "next" ? next : prev
-
-        let nextDoc;
-        let prevDoc;
-
-        let response = await axios({
-            method  : 'get',
-            url     : '/api',
-            params  : {
-                'doc_id' : docId ,
-                'type'   : type
-            },
-            headers : {
-                'Content-Type' : 'multipart/form-data'
-            },
-        })
-
-        var data = response.data.rows
-
-        //이전 페이지를 호출 할 경우
-        if(type == "prev"){
-            prevTarget++//이전페이지 카운트 증가
-
-            //첫페이지에서 이전 페이지 호출(이전페이지 카운트가 저장 리스트의 길이와 같을 경우)
-            if(prevTarget == docList.length - 1 ){
-                prevTarget--//이전페이지 카운트 감소(이후 이전 페이지 호출시에도 첫페이지를 호출 하기 위함)
-            }
-
-        }//다음 페이지를 호출 할 경우
-        else if(type == "next" || type == ""){
-            docList.push(data[0].doc_id)//호출한 페이지의 문서 아이디값 추가
-            docListId.push(data[0].id)//호출한 페이지의 문서 인덱스값 추가
-
-            //배열 중복 제거(만약 다음 페이지가 이전페이지 호출등의 이유로 이미 호출 했을 경우 배열에 쌓지 않기 위함)
-            const set1 = new Set(docList);
-            docList = [...set1];
-            const set2 = new Set(docListId);
-            docListId = [...set2];
-            
-            if(prevTarget != 0 ) {//첫페이지가 아닐 경우
-                prevTarget-- //이전페이지 카운트 감소
-            }
-        }
-
-        //다음페이지 눌렸을때 게시물 수가 countPerPage 이하힐때 마지막페이지로 간주
-        if(type == "next" && data.length < 10){  
-            nextDoc = data[0].doc_id
-            prevDoc = type == "" ? docList[0] : docList[docList.length - (prevTarget + 2)]
-        }
-        else{//마지막 페이지가 아닐때
-            nextDoc = data[Number(data.length-1)].doc_id
-            prevDoc = type == "" ? docList[0] : docList[docList.length - (prevTarget + 2)]
-        }
-        
-        // console.log("prevTarget = " + prevTarget)
-        // console.log(docListId)
-
-        setPrev(prevDoc)
-        setNext(nextDoc)
-        setList(data);
-    }
-
-
-
-
-
-
-
-
-    //리스트 가져오기
-    const getMovieList22 = async (useParams, e) => {
-        
-        let type       = useParams.type == undefined ? '' : useParams.type 
+        let type       = useParams.type       == undefined ? '' : useParams.type 
+        let docList    = useParams.docList    == undefined ? [] : useParams.docList
+        let prevTarget = useParams.prevTarget == undefined ? 0  : useParams.prevTarget;
         let docId      = useParams.docId;
-        let docList    = useParams.docList == undefined ? [] : useParams.docList
-        let prevTarget = useParams.prevTarget == undefined ? 0 : useParams.prevTarget;
+        
 
         let response = await axios({
             method  : 'get',
@@ -125,24 +49,17 @@ function MovieList() {
         
         var data = response.data.rows
 
-        var pagingArr = pagination(data, type, docList, prevTarget );
-        
-        console.log(pagingArr[0])
-        console.log(pagingArr[1])
-        console.log(pagingArr[2])
-        console.log(pagingArr[3])
-        console.log(docListId)
+        var pagingArr = Pagination(data, type, docList, prevTarget);
 
         setPrev(pagingArr[0])
         setNext(pagingArr[1])
         setDocList(pagingArr[2])
         setPrevTarget(pagingArr[3])
         setList(data);
-
     }
 
     useEffect(() => {
-        getMovieList22({});
+        getMovieList({});
     },  []);
 
     const url = '/view/movieDetail/'
@@ -160,76 +77,21 @@ function MovieList() {
                                 {/* 
                                     랜덤 이미지 링크 1, 2
                                     <img src={"https://source.unsplash.com/random/200x200?sig="+index} alt ="not exist" />
-                                    <img src={"https://picsum.photos/200/300?random="+index} alt ="not exist" /> 
-                                */}
+                                    <img src={"https://picsum.photos/200/300?random="+index} alt ="not exist" />*/}
                                 <p className="card-text">{ list.tagline !== "" ? list.tagline : " - " }</p>                        
                             </div>
                         </div>
                     </a>    
                 ))}
-
-                <div className="pagination_wrap">
-                    {/* <Pagination useParams={{ data : 'list', url : "/api"}}></Pagination> */}
-                    
-                    
+                <div className="pagination_wrap">      
                     <ButtonGroup className="pagination">
-                        <ToggleButton onClick={(e)=>{getMovieList22({ type : 'prev', docId : prev, docList : docList, prevTarget : prevTarget}, e)}} type="radio"variant={'outline-success'} name="radio"> &larr; 이전 </ToggleButton>
-                        <ToggleButton onClick={(e)=>{getMovieList22({ type : 'next', docId : next, docList : docList, prevTarget : prevTarget}, e)}} type="radio"variant={'outline-primary'} name="radio"> 다음 &rarr; </ToggleButton>
+                        <ToggleButton onClick={(e)=>{getMovieList({ type : 'prev', docId : prev, docList : docList, prevTarget : prevTarget}, e)}} type="radio"variant={'outline-success'} name="radio"> &larr; 이전 </ToggleButton>
+                        <ToggleButton onClick={(e)=>{getMovieList({ type : 'next', docId : next, docList : docList, prevTarget : prevTarget}, e)}} type="radio"variant={'outline-primary'} name="radio"> 다음 &rarr; </ToggleButton>
                     </ButtonGroup>  
-                    
-                   
                 </div>
             </div>                
         </React.Fragment>
     )
 }
-
-
-function pagination (data, type, docList, prevTarget ) {
-    
-    let prevDoc = "PPP";
-    let nextDoc = "NNN";
-    
-    //이전 페이지를 호출 할 경우
-    if(type == "prev"){
-        prevTarget++//이전페이지 카운트 증가
-
-        //첫페이지에서 이전 페이지 호출(이전페이지 카운트가 저장 리스트의 길이와 같을 경우)
-        if(prevTarget == docList.length - 1 ){
-            prevTarget--//이전페이지 카운트 감소(이후 이전 페이지 호출시에도 첫페이지를 호출 하기 위함)
-        }
-
-    }//다음 페이지를 호출 할 경우
-    else if(type == "next" || type == ""){
-        docList.push(data[0].doc_id)//호출한 페이지의 문서 아이디값 추가
-
-        //배열 중복 제거(만약 다음 페이지가 이전페이지 호출등의 이유로 이미 호출 했을 경우 배열에 쌓지 않기 위함)
-        const set1 = new Set(docList);
-        docList = [...set1];
-
-        docListId.push(data[0].id)//호출한 페이지의 문서 아이디값 추가
-        const set2 = new Set(docListId);
-        docListId = [...set2];
-
-        if(prevTarget != 0 ) {//첫페이지가 아닐 경우
-            prevTarget-- //이전페이지 카운트 감소
-        }
-    }
-    //다음페이지 눌렸을때 게시물 수가 countPerPage 이하힐때 마지막페이지로 간주
-    if(type == "next" && data.length < 10){  
-        nextDoc = data[0].doc_id
-        prevDoc = type == "" ? docList[0] : docList[docList.length - (prevTarget + 2)]
-    }
-    else{//마지막 페이지가 아닐때
-        nextDoc = data[Number(data.length-1)].doc_id
-        prevDoc = type == "" ? docList[0] : docList[docList.length - (prevTarget + 2)]
-    }
-    
-    let pagingArr = [prevDoc, nextDoc, docList, prevTarget]
-
-    return pagingArr;
-
-}
-
 
 export default MovieList;
