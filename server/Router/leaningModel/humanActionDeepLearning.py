cd001aee20209b5a3e32e015eaaca85664bc797c
cho younghyun <5869jyh@hanmail.net>
2022-11-09 17:25:12 +0900

    Work_Audio DeepLearning Sample

--- server/Router/leaningModel/humanActionDeepLearning.py
+++ server/Router/leaningModel/humanActionDeepLearning.py
@@ -35,7 +35,7 @@ from webdriver_manager.chrome          import ChromeDriverManager
 from selenium.webdriver.common.by      import By
 
 dn_url = 'C:/Users/all4land/.keras/datasets/animalSound/'
-#tesorflow_io
+# tesorflow_io, tensorflowr 호환 버전 확인 URL : https://pypi.org/project/tensorflow-io/
 yamnet_model_handle = 'https://tfhub.dev/google/yamnet/1'
 yamnet_model = hub.load(yamnet_model_handle)
 
@@ -63,10 +63,10 @@ def load_wav_16k_mono(filename):
 
 testing_wav_data = load_wav_16k_mono(testing_wav_file_name)
 
-plt.plot(testing_wav_data)
-plt.show()
+# plt.plot(testing_wav_data)
+# plt.show()
 # Play the audio file.
-display.Audio(testing_wav_data,rate=16000)
+# display.Audio(testing_wav_data,rate=16000)
 
 
 
@@ -90,17 +90,224 @@ print(f'The main sound is: {inferred_class}')
 print(f'The embeddings shape: {embeddings.shape}')
 
 
-tf.keras.utils.get_file('esc-50.zip',
-                        'https://github.com/karoldvl/ESC-50/archive/master.zip',
-                        cache_dir=dn_url,
-                        cache_subdir='datasets',
-                        extract=True)
+# 링크로 들어가서 수동으로 다운 받기
+# audio_data = tf.keras.utils.get_file('master.zip',
+#                         'https://github.com/karoldvl/ESC-50/archive/master.zip',
+#                         cache_dir = dn_url,
+#                         cache_subdir = 'datasets')
+
+
+
+esc50_csv = dn_url + 'datasets/ESC-50-master/meta/esc50.csv'
+base_data_path = dn_url + 'datasets/ESC-50-master/audio/'
+
+pd_data = pd.read_csv(esc50_csv)
+pd_data.head()
+
+print(pd_data.head())
+
+
+
+my_classes = ['dog', 'cat']
+map_class_to_id = {'dog':0, 'cat':1}
+
+filtered_pd = pd_data[pd_data.category.isin(my_classes)]
+
+class_id = filtered_pd['category'].apply(lambda name: map_class_to_id[name])
+filtered_pd = filtered_pd.assign(target=class_id)
+
+full_path = filtered_pd['filename'].apply(lambda row: os.path.join(base_data_path, row))
+filtered_pd = filtered_pd.assign(filename=full_path)
+
+print(filtered_pd.head(10))
 
 
 
 
-# esc50_csv = dn_url +'datasets/ESC-50-master/meta/esc50.csv'
-# base_data_path = dn_url + 'datasets/ESC-50-master/audio/'
 
-# pd_data = pd.read_csv(esc50_csv)
-# pd_data.head()
+
+
+
+filenames = filtered_pd['filename']
+targets = filtered_pd['target']
+folds = filtered_pd['fold']
+
+main_ds = tf.data.Dataset.from_tensor_slices((filenames, targets, folds))
+main_ds.element_spec
+
+
+
+
+
+def load_wav_for_map(filename, label, fold):
+  return load_wav_16k_mono(filename), label, fold
+
+main_ds = main_ds.map(load_wav_for_map)
+print(main_ds.element_spec)
+
+
+print("--------------------------------------------------------------------------------------------")
+
+
+# applies the embedding extraction model to a wav data
+def extract_embedding(wav_data, label, fold):
+  ''' run YAMNet to extract embedding from the wav data '''
+  scores, embeddings, spectrogram = yamnet_model(wav_data)
+  num_embeddings = tf.shape(embeddings)[0]
+  return (embeddings,
+            tf.repeat(label, num_embeddings),
+            tf.repeat(fold, num_embeddings))
+
+# extract embedding
+main_ds = main_ds.map(extract_embedding).unbatch()
+print(main_ds.element_spec)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+cached_ds = main_ds.cache()
+train_ds = cached_ds.filter(lambda embedding, label, fold: fold < 4)
+val_ds = cached_ds.filter(lambda embedding, label, fold: fold == 4)
+test_ds = cached_ds.filter(lambda embedding, label, fold: fold == 5)
+
+# remove the folds column now that it's not needed anymore
+remove_fold_column = lambda embedding, label, fold: (embedding, label)
+
+train_ds = train_ds.map(remove_fold_column)
+val_ds = val_ds.map(remove_fold_column)
+test_ds = test_ds.map(remove_fold_column)
+
+train_ds = train_ds.cache().shuffle(1000).batch(32).prefetch(tf.data.AUTOTUNE)
+val_ds = val_ds.cache().batch(32).prefetch(tf.data.AUTOTUNE)
+test_ds = test_ds.cache().batch(32).prefetch(tf.data.AUTOTUNE)
+
+print("--------------------------------------------------------------------------------------------")
+
+print(test_ds)
+
+
+
+
+my_model = tf.keras.Sequential([
+    tf.keras.layers.Input(shape=(1024), dtype=tf.float32,
+                          name='input_embedding'),
+    tf.keras.layers.Dense(512, activation='relu'),
+    tf.keras.layers.Dense(len(my_classes))
+], name='my_model')
+
+my_model.summary()
+
+
+
+my_model.compile(loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                 optimizer="adam",
+                 metrics=['accuracy'])
+
+callback = tf.keras.callbacks.EarlyStopping(monitor='loss',
+                                            patience=3,
+                                            restore_best_weights=True)
+
+history = my_model.fit(train_ds,
+                       epochs=20,
+                       validation_data=val_ds,
+                       callbacks=callback)
+
+
+loss, accuracy = my_model.evaluate(test_ds)
+
+print("Loss: ", loss)
+print("Accuracy: ", accuracy)
+
+
+
+
+scores, embeddings, spectrogram = yamnet_model(testing_wav_data)
+result = my_model(embeddings).numpy()
+
+inferred_class = my_classes[result.mean(axis=0).argmax()]
+print(f'The main sound is: {inferred_class}')
+
+
+
+
+
+class ReduceMeanLayer(tf.keras.layers.Layer):
+  def __init__(self, axis=0, **kwargs):
+    super(ReduceMeanLayer, self).__init__(**kwargs)
+    self.axis = axis
+
+  def call(self, input):
+    return tf.math.reduce_mean(input, axis=self.axis)
+
+# saved_model_path = dn_url + 'miaow_16k.wav'
+
+saved_model_path = dn_url + 'model/dogs_and_cats_yamnet'
+
+input_segment = tf.keras.layers.Input(shape=(), dtype=tf.float32, name='audio')
+embedding_extraction_layer = hub.KerasLayer(yamnet_model_handle,
+                                            trainable=False, name='yamnet')
+_, embeddings_output, _ = embedding_extraction_layer(input_segment)
+serving_outputs = my_model(embeddings_output)
+serving_outputs = ReduceMeanLayer(axis=0, name='classifier')(serving_outputs)
+serving_model = tf.keras.Model(input_segment, serving_outputs)
+serving_model.save(saved_model_path, include_optimizer=False)
+
+
+
+
+
+
+
+
+reloaded_model = tf.saved_model.load(saved_model_path)
+
+reloaded_results = reloaded_model(testing_wav_data)
+cat_or_dog = my_classes[tf.argmax(reloaded_results)]
+print(f'The main sound is: {cat_or_dog}')
+
+
+
+
+
+
+
+
+
+test_pd = filtered_pd.loc[filtered_pd['fold'] == 5]
+row = test_pd.sample(1)
+filename = row['filename'].item()
+print(filename)
+waveform = load_wav_16k_mono(filename)
+print(f'Waveform values: {waveform}')
+_ = plt.plot(waveform)
+
+display.Audio(waveform, rate=16000)
+
+
+
+# Run the model, check the output.
+scores, embeddings, spectrogram = yamnet_model(waveform)
+class_scores = tf.reduce_mean(scores, axis=0)
+top_class = tf.argmax(class_scores)
+inferred_class = class_names[top_class]
+top_score = class_scores[top_class]
+print(f'[YAMNet] The main sound is: {inferred_class} ({top_score})')
+
+reloaded_results = reloaded_model(waveform)
+your_top_class = tf.argmax(reloaded_results)
+your_inferred_class = my_classes[your_top_class]
+class_probabilities = tf.nn.softmax(reloaded_results, axis=-1)
+your_top_score = class_probabilities[your_top_class]
+print(f'[Your model] The main sound is: {your_inferred_class} ({your_top_score})')
