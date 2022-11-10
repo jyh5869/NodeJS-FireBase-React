507d7502302440d5e68e33675f3cf1d14960c205
cho younghyun <5869jyh@hanmail.net>
2022-11-10 17:49:37 +0900

    Work_오디오 딥러닝마무리

--- server/Router/sampleModel/audioDeepLearningType1.py
+++ server/Router/sampleModel/audioDeepLearningType1.py
@@ -4,7 +4,6 @@ import re
 import os
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-# https://machinelearningmastery.com/image-augmentation-with-keras-preprocessing-layers-and-tf-image/
 
 import json
 import cv2
@@ -34,7 +33,9 @@ from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome          import ChromeDriverManager
 from selenium.webdriver.common.by      import By
 
-# ※참고사항 : tesorflow_io, tensorflowr 호환 버전 확인 필요 (URL : https://pypi.org/project/tensorflow-io/)
+# ※참고사항 : tesorflow_io, tensorflowr 호환 버전 확인 필요 (URL : pypi.org/project/tensorflow-io/)
+# ※참고링크 : www.tensorflow.org/tutorials/audio/transfer_learning_audio?hl=ko
+
 
 
 dn_url = 'C:/Users/all4land/.keras/datasets/animalSound/'
@@ -57,7 +58,7 @@ testing_wav_file_name = tf.keras.utils.get_file(
 
 # 오디오 파일 로드하는 함수
 def load_wav_16k_mono(filename):
-    """ Load a WAV file, convert it to a float tensor, resample to 16 kHz single-channel audio. """
+
     file_contents = tf.io.read_file(filename)
     
     wav, sample_rate = tf.audio.decode_wav(
@@ -92,11 +93,12 @@ class_names = list(pd.read_csv(class_map_path)['display_name'])
 
 
 scores, embeddings, spectrogram = yamnet_model(testing_wav_data)
-class_scores = tf.reduce_mean(scores, axis=0)
-top_class = tf.argmax(class_scores)
+
+class_scores   = tf.reduce_mean(scores, axis=0)
+top_class      = tf.argmax(class_scores)
 inferred_class = class_names[top_class]
 
-print(f'The main sound is: {inferred_class}')
+print(f'The main sound is:    {inferred_class  }')
 print(f'The embeddings shape: {embeddings.shape}')
 
 
@@ -115,7 +117,6 @@ base_data_path = dn_url + 'datasets/ESC-50-master/audio/'
 
 pd_data = pd.read_csv(esc50_csv)
 pd_data.head()
-
 print(pd_data.head())
 
 
@@ -130,7 +131,6 @@ filtered_pd = filtered_pd.assign(target = class_id)
 
 full_path   = filtered_pd['filename'].apply(lambda row : os.path.join(base_data_path, row))
 filtered_pd = filtered_pd.assign(filename = full_path)
-
 print(filtered_pd.head(10))
 
 
@@ -148,63 +148,51 @@ def load_wav_for_map(filename, label, fold):
     return load_wav_16k_mono(filename), label, fold
 
 main_ds = main_ds.map(load_wav_for_map)
-
 print(main_ds.element_spec)
-print("--------------------------------------------------------------------------------------------")
+print("---------------------------------------------------------------")
 
 
 
-# applies the embedding extraction model to a wav data
+# 자연어 처리(백터화 임베딩)
 def extract_embedding(wav_data, label, fold):
-  ''' run YAMNet to extract embedding from the wav data '''
-  scores, embeddings, spectrogram = yamnet_model(wav_data)
-  num_embeddings = tf.shape(embeddings)[0]
-  return (embeddings,
-            tf.repeat(label, num_embeddings),
-            tf.repeat(fold, num_embeddings))
+    
+    scores, embeddings, spectrogram = yamnet_model(wav_data)
+    num_embeddings = tf.shape(embeddings)[0]
+    return(
+              embeddings,
+              tf.repeat(label, num_embeddings),
+              tf.repeat(fold, num_embeddings)
+          )
 
-# extract embedding
+# 임베딩된 데이터셋 추출
 main_ds = main_ds.map(extract_embedding).unbatch()
 print(main_ds.element_spec)
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 cached_ds = main_ds.cache()
-train_ds = cached_ds.filter(lambda embedding, label, fold: fold < 4)
-val_ds = cached_ds.filter(lambda embedding, label, fold: fold == 4)
-test_ds = cached_ds.filter(lambda embedding, label, fold: fold == 5)
+train_ds  = cached_ds.filter(lambda embedding, label, fold: fold <  4)
+val_ds    = cached_ds.filter(lambda embedding, label, fold: fold == 4)
+test_ds   = cached_ds.filter(lambda embedding, label, fold: fold == 5)
 
-# remove the folds column now that it's not needed anymore
+# 불필요한 폴드 데이터 삭제 및 데이터셋 세팅
 remove_fold_column = lambda embedding, label, fold: (embedding, label)
+print('-remove_fold_column-')
+print(remove_fold_column)
 
 train_ds = train_ds.map(remove_fold_column)
-val_ds = val_ds.map(remove_fold_column)
-test_ds = test_ds.map(remove_fold_column)
+val_ds   = val_ds.map(remove_fold_column)
+test_ds  = test_ds.map(remove_fold_column)
 
 train_ds = train_ds.cache().shuffle(1000).batch(32).prefetch(tf.data.AUTOTUNE)
-val_ds = val_ds.cache().batch(32).prefetch(tf.data.AUTOTUNE)
-test_ds = test_ds.cache().batch(32).prefetch(tf.data.AUTOTUNE)
-
-print("--------------------------------------------------------------------------------------------")
-
+val_ds   = val_ds.cache().batch(32).prefetch(tf.data.AUTOTUNE)
+test_ds  = test_ds.cache().batch(32).prefetch(tf.data.AUTOTUNE)
+print("-training dataset-")
 print(test_ds)
 
 
 
-
+# 모델 생성 및 훈련
 my_model = tf.keras.Sequential([
     tf.keras.layers.Input(shape=(1024), dtype=tf.float32,
                           name='input_embedding'),
@@ -213,8 +201,6 @@ my_model = tf.keras.Sequential([
 ], name='my_model')
 
 my_model.summary()
-
-
 
 my_model.compile(loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
                  optimizer="adam",
@@ -225,19 +211,18 @@ callback = tf.keras.callbacks.EarlyStopping(monitor='loss',
                                             restore_best_weights=True)
 
 history = my_model.fit(train_ds,
-                       epochs=20,
-                       validation_data=val_ds,
-                       callbacks=callback)
+                      epochs=20,
+                      validation_data=val_ds,
+                      callbacks=callback)
 
 
 loss, accuracy = my_model.evaluate(test_ds)
-
-print("Loss: ", loss)
+print("Loss:     ", loss    )
 print("Accuracy: ", accuracy)
 
 
 
-
+# 모델 테스트
 scores, embeddings, spectrogram = yamnet_model(testing_wav_data)
 result = my_model(embeddings).numpy()
 
@@ -247,17 +232,16 @@ print(f'The main sound is: {inferred_class}')
 
 
 
-
 class ReduceMeanLayer(tf.keras.layers.Layer):
-  def __init__(self, axis=0, **kwargs):
-    super(ReduceMeanLayer, self).__init__(**kwargs)
-    self.axis = axis
+    def __init__(self, axis=0, **kwargs):
+        super(ReduceMeanLayer, self).__init__(**kwargs)
+        self.axis = axis
 
-  def call(self, input):
-    return tf.math.reduce_mean(input, axis=self.axis)
+    def call(self, input):
+        return tf.math.reduce_mean(input, axis=self.axis)
 
-# saved_model_path = dn_url + 'miaow_16k.wav'
 
+# 훈련된 모델 저장 (h5 or directory)
 saved_model_path = dn_url + 'model/dogs_and_cats_yamnet'
 
 input_segment = tf.keras.layers.Input(shape=(), dtype=tf.float32, name='audio')
@@ -266,53 +250,48 @@ embedding_extraction_layer = hub.KerasLayer(yamnet_model_handle,
 _, embeddings_output, _ = embedding_extraction_layer(input_segment)
 serving_outputs = my_model(embeddings_output)
 serving_outputs = ReduceMeanLayer(axis=0, name='classifier')(serving_outputs)
-serving_model = tf.keras.Model(input_segment, serving_outputs)
+serving_model   = tf.keras.Model(input_segment, serving_outputs)
 serving_model.save(saved_model_path, include_optimizer=False)
 
+# 모델 계층 시각화 (모델의 구조를 이미지로 확인)
+tf.keras.utils.plot_model(serving_model, show_shapes=True)
 
 
-
-tf.keras.utils.plot_model(serving_model)
-
-
-
-reloaded_model = tf.saved_model.load(saved_model_path)
-
+# 위의 저장된 모델 호출
+reloaded_model   = tf.saved_model.load(saved_model_path)
 reloaded_results = reloaded_model(testing_wav_data)
+
 cat_or_dog = my_classes[tf.argmax(reloaded_results)]
 print(f'The main sound is: {cat_or_dog}')
 
 
-
-
-
-
-
-
-
+# 검증 데이터 세팅
 test_pd = filtered_pd.loc[filtered_pd['fold'] == 5]
 row = test_pd.sample(1)
 filename = row['filename'].item()
-print(filename)
 waveform = load_wav_16k_mono(filename)
-print(f'Waveform values: {waveform}')
-_ = plt.plot(waveform)
-
+plt.plot(waveform)
 display.Audio(waveform, rate=16000)
+# print(f'Waveform values: {waveform}')
+# print(filename)
 
 
 
-# Run the model, check the output.
+# 검증데이터를 YAMNET과 훈련모델에서 분석 및 결과 비교
 scores, embeddings, spectrogram = yamnet_model(waveform)
-class_scores = tf.reduce_mean(scores, axis=0)
-top_class = tf.argmax(class_scores)
+class_scores   = tf.reduce_mean(scores, axis=0)
+top_class      = tf.argmax(class_scores)
 inferred_class = class_names[top_class]
-top_score = class_scores[top_class]
+top_score      = class_scores[top_class]
 print(f'[YAMNet] The main sound is: {inferred_class} ({top_score})')
 
-reloaded_results = reloaded_model(waveform)
-your_top_class = tf.argmax(reloaded_results)
+reloaded_results    = reloaded_model(waveform)
+your_top_class      = tf.argmax(reloaded_results)
 your_inferred_class = my_classes[your_top_class]
 class_probabilities = tf.nn.softmax(reloaded_results, axis=-1)
-your_top_score = class_probabilities[your_top_class]
-print(f'[Your model] The main sound is: {your_inferred_class} ({your_top_score})')
+your_top_score      = class_probabilities[your_top_class]
+print(f'[My model] The main sound is: {your_inferred_class} ({your_top_score})')
+
+
+
+
