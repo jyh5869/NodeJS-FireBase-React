6ddb289ba61c473b58eceb7c9af27096240957ae
jyh5869 <5869jyh@hanmail.net>
2023-06-05 17:13:59 +0900

    Home_오픈레이어스 실습

--- client/src/view/common/map.js
+++ client/src/view/common/map.js
@@ -12,6 +12,12 @@ import GeoJSON from 'ol/format/GeoJSON.js';
 import View from 'ol/View.js';
 import XYZ from 'ol/source/XYZ';
 
+import {Draw, Modify, Snap} from 'ol/interaction.js';
+import {GeometryCollection, Point, Polygon} from 'ol/geom.js';
+import {circular} from 'ol/geom/Polygon.js';
+import {getDistance} from 'ol/sphere.js';
+import {transform} from 'ol/proj.js';
+
 import {OSM, Vector as VectorSource} from 'ol/source.js';
 import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer.js';
 
@@ -99,6 +105,128 @@ function Map1({}) {
         view.centerOn(point.getCoordinates(), size, [570, 500]);
     };
 
+
+
+
+
+
+
+
+
+    const defaultStyle = new Modify({source: source})
+        .getOverlay()
+        .getStyleFunction();
+
+    const modify = new Modify({
+        source: source,
+        style: function (feature) {
+            feature.get('features').forEach(function (modifyFeature) {
+            const modifyGeometry = modifyFeature.get('modifyGeometry');
+            if (modifyGeometry) {
+                const modifyPoint = feature.getGeometry().getCoordinates();
+                const geometries = modifyFeature.getGeometry().getGeometries();
+                const polygon = geometries[0].getCoordinates()[0];
+                const center = geometries[1].getCoordinates();
+                const projection = map.getView().getProjection();
+                let first, last, radius;
+                if (modifyPoint[0] === center[0] && modifyPoint[1] === center[1]) {
+                // center is being modified
+                // get unchanged radius from diameter between polygon vertices
+                first = transform(polygon[0], projection, 'EPSG:4326');
+                last = transform(
+                    polygon[(polygon.length - 1) / 2],
+                    projection,
+                    'EPSG:4326'
+                );
+                radius = getDistance(first, last) / 2;
+                } else {
+                // radius is being modified
+                first = transform(center, projection, 'EPSG:4326');
+                last = transform(modifyPoint, projection, 'EPSG:4326');
+                radius = getDistance(first, last);
+                }
+                // update the polygon using new center or radius
+                const circle = circular(
+                transform(center, projection, 'EPSG:4326'),
+                radius,
+                128
+                );
+                circle.transform('EPSG:4326', projection);
+                geometries[0].setCoordinates(circle.getCoordinates());
+                // save changes to be applied at the end of the interaction
+                modifyGeometry.setGeometries(geometries);
+            }
+            });
+            return defaultStyle(feature);
+        },
+    });
+
+    modify.on('modifystart', function (event) {
+        console.log("수정 시작");
+        event.features.forEach(function (feature) {
+            const geometry = feature.getGeometry();
+            if (geometry.getType() === 'GeometryCollection') {
+            feature.set('modifyGeometry', geometry.clone(), true);
+            }
+        });
+    });
+
+    modify.on('modifyend', function (event) {
+        console.log("수정 종료");
+        event.features.forEach(function (feature) {
+            const modifyGeometry = feature.get('modifyGeometry');
+            if (modifyGeometry) {
+            feature.setGeometry(modifyGeometry);
+            feature.unset('modifyGeometry', true);
+            }
+        });
+    });
+
+    //지도 초기화
+    const initMap = async (e) => {
+        console.log(e.target.value)
+        map.removeInteraction(draw);
+        map.removeInteraction(snap);
+        addInteractions(e);
+        
+    };
+
+    let draw, snap; // global so we can remove them later
+    function addInteractions(e) {
+        
+        let value = e.target.value;
+        let geometryFunction;
+        if (value === 'Geodesic') {
+            value = 'Circle';
+            geometryFunction = function (coordinates, geometry, projection) {
+            if (!geometry) {
+                geometry = new GeometryCollection([
+                new Polygon([]),
+                new Point(coordinates[0]),
+                ]);
+            }
+            const geometries = geometry.getGeometries();
+            const center = transform(coordinates[0], projection, 'EPSG:4326');
+            const last = transform(coordinates[1], projection, 'EPSG:4326');
+            const radius = getDistance(center, last);
+            const circle = circular(center, radius, 128);
+            circle.transform('EPSG:4326', projection);
+            geometries[0].setCoordinates(circle.getCoordinates());
+            geometry.setGeometries(geometries);
+            return geometry;
+            };
+        }
+        draw = new Draw({
+            source: source,
+            type: value,
+            geometryFunction: geometryFunction,
+        });
+        map.addInteraction(draw);
+        snap = new Snap({source: source});
+        map.addInteraction(snap);
+        
+    }
+
     return (
         <>
             <link rel="stylesheet" href="node_modules/ol/ol.css"></link>
@@ -108,6 +236,16 @@ function Map1({}) {
             <button id="centerlausanne" onClick={(e) => { CenterOnLausanne();}}>Center on Lausanne</button><br/><br/>
             <button id="zoom-out" onClick={(e) => { handleClick('zoomOut');}}>Zoom out</button>
             <button id="zoom-in" onClick={(e) => { handleClick('zoomIn');}}>Zoom in</button>
+
+            <form>
+            <label for="type">Geometry type &nbsp;</label>
+            <select id="type" onChange={(e) => { initMap(e);}}>
+                <option value="Point">Point</option>
+                <option value="LineString">LineString</option>
+                <option value="Polygon">Polygon</option>
+                <option value="Circle">Circle</option>
+            </select>
+            </form>
         </>
     );
 }
